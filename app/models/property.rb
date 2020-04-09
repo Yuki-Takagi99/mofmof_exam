329923d76cc64d472e6b2f9ab9704f87d5283c81
yuki takagi <yuki99takagi@gmail.com>
2020-04-09 10:55:04 +0900

    バリデーション設定

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,6 +1,12 @@
 class Property < ApplicationRecord
   has_many :nearest_stations, dependent: :destroy
   accepts_nested_attributes_for :nearest_stations, reject_if: :reject_both_blank, allow_destroy: true
+  #reject_ifで指定した条件に合致した場合、子要素を作成しない
+  #arrow_destroy trueで削除可能にする
+  validates :property_name, presence: true, length: { minimum: 1, maximum: 255 }
+  validates :rental_price, presence: true
+  validates :address, presence: true, length: { minimum: 1, maximum: 255 }
+  validates :age, presence: true
 
   def reject_both_blank(attributes)
     if attributes[:id]
